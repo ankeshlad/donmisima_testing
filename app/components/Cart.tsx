09acc68d0068620456584214cb683237169ecd50
Ankesh Lad <76177255+ankeshlad@users.noreply.github.com>
2024-06-14 11:51:09 +0530

    worked on checkout btn

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -155,7 +155,7 @@ function CartCheckoutActions({checkoutUrl}: {checkoutUrl: string}) {
   return (
     <div className="cart-checkout-button-wrapper">
       <a className="cart-checkout-button" href={checkoutUrl} target="_self">
-        <p>Checkout</p>
+        <p>Checkout 1</p>
       </a>
     </div>
   );
