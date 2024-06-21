36e7302db685c25e3fc2320e091e82805af6a0c1
Ankesh Lad <76177255+ankeshlad@users.noreply.github.com>
2024-06-21 15:59:06 +0530

    changes done

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -154,9 +154,6 @@ function CartCheckoutActions({checkoutUrl}: {checkoutUrl: string}) {
 
   return (
     <div className="cart-checkout-button-wrapper">
-      {/* <a className="cart-checkout-button" href={checkoutUrl} target="_self">
-        <p>Checkout 1</p>
-      </a> */}
       <a className="cart-checkout-button" href={checkoutUrl} target="_self">
         <p>Place Order</p>
       </a>
